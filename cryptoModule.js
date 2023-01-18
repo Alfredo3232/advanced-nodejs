cec4ae78c837c95771292185005adac489411773
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-01-18 12:44:47 -0800

    add comments, and testing more event loop

--- cryptoModule.js
+++ cryptoModule.js
@@ -2,10 +2,30 @@ const crypto = require('crypto');
 
 const start = Date.now();
 
+// 1 Seconds
 crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
     console.log('1: ', Date.now() - start);
 });
 
 crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
     console.log('2: ', Date.now() - start);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('3: ', Date.now() - start);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('4: ', Date.now() - start);
+});
+// 1 Seconds
+
+/* 
+    These all roughly took the same amount of time however on the next one, the time will jump
+    drastically
+*/
+
+// 2 Seconds
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('5: ', Date.now() - start);
 });
