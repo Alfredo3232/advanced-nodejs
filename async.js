b90a406e787f8545ac191cbad199cf1ffd484d54
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-01-19 10:41:33 -0800

    simple http request

---
+++ async.js
@@ -0,0 +1,10 @@
+const https = require('https');
+
+const start = Date.now();
+
+https.request('https://www.google.com', (res) => {
+    res.on('data', () => { });
+    res.on('end', () => {
+        console.log(Date.now() - start);
+    });
+});
